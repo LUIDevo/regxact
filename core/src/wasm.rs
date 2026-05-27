d7c26def2fc73f5d72721b20b70d3586ff37295e
louis <sanicisspeedandfast@gmail.com>
2026-05-26 23:15:20 -0400

    updated WASM struct with functions

--- core/src/wasm.rs
+++ core/src/wasm.rs
@@ -4,3 +4,29 @@ use crate::rx::Rx;
 
 #[wasm_bindgen]
 pub struct WasmRegxact {inner: Rx}
+
+#[wasm_bindgen]
+impl WasmRegxact {
+
+    #[wasm_bindgen(constructor)]
+    pub fn new(pattern: &str) ->
+        Result<WasmRegxact, JsError> {
+            RegxactBuilder::new(pattern).build()
+                .map(|inner| WasmRegxact { inner })
+                .map_err(|e|
+                    JsError::new(&format!("{e:?}")))
+    }
+
+    pub fn allow(mut self, flag: &str) ->
+        Result<WasmRegxact, JsError> {
+            self.inner =
+                self.inner.allow(flag).map_err(|e|
+                    JsError::new(&format!("{e:?}")))?;
+            Ok(self)
+    }
+
+    #[wasm_bindgen(getter)]
+    pub fn pattern(&self) -> String {
+        self.inner.pattern.clone() 
+    }
+}
