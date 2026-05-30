659bcd628b4e27972d59a951f696ca4095f83b0d
louis <sanicisspeedandfast@gmail.com>
2026-05-30 01:45:38 -0400

    added dependencies to wasm dir

--- bindings/wasm/src/lib.rs
+++ bindings/wasm/src/lib.rs
@@ -1,8 +1,15 @@
 // build the json, handle Ok, and error 
 // handle macros
-
+use regxact_core::Rx;
 
 #[wasm_bindgen]
 pub fn create(pattern: &str) -> String {
     to_json(Rx::create(pattern))
+}
+
+fn to_json(result: Result<Rx, RegxactError>) -> String{
+    match result {
+        // Ok(Rx)=>serde_json::to_string()
+        
+    }
 }
